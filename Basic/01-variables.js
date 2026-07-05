79f8d80ba39e76504fb27d6ababa7dd85d135b79
Laura Kamila <bonnielaura534@gmail.com>
2026-07-05 16:24:40 -0500

    variables

--- Basic/01-variables.js
+++ Basic/01-variables.js
@@ -27,3 +27,31 @@ console.log(helloWorld3)
 // Error
 // helloWorld3 = "¡Hola de nuevo, JavaScript 2!"
 // console.log(helloWorld3)
+
+
+// EJERCICIOS LAURA
+
+// var
+
+var helloWorld = '¡Hello, JavaScript!'
+console.log(helloWorld)
+
+helloWorld = '¡Hello again, JavaScript!'
+console.log(helloWorld) //Forma antigua de usar variables
+
+// let
+
+let helloWorld2 = '¡Hello, JavaScript 2!'
+console.log(helloWorld2)
+
+helloWorld2 = '¡Hello, JavaScript 2!'
+console.log(helloWorld2)
+
+// const
+
+const helloWorld3 = 'Hello, JavaScript 3'
+console.log(helloWorld3)
+
+helloWorld3 = 'Hello again, JavaScript 3'   
+console.log(helloWorld3) //Forma moderna de usar variables  
+
